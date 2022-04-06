119d5f12cbe9983864592818d3d2b77a964e39b0
scottedkreider <scottedkreider@gmail.com>
2022-04-05 20:07:00 -0400

    Signed-off-by: scottedkreider <scottedkreider@gmail.com>

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 // Load Node modules
 var express = require('express');
 const ejs = require('ejs');
+const path = require('path');
 
 // Initialise Express
 var app = express();
 
 // Render static files
 console.log(__dirname + '/public');
-app.use(express.static(__dirname + '/public'));
+app.use("/public",express.static(path.join(__dirname,'/public')));
 
 // Set the view engine to ejs
 app.set('view engine', 'ejs');
